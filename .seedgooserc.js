ee91d9cdaf9b5b645da73de8ff75f42056b0f9cf
Matt Smith <moonlitaltar@protonmail.com>
2023-09-26 07:30:48 -0400

    Module 4

--- .seedgooserc.js
+++ .seedgooserc.js
@@ -7,5 +7,5 @@ module.exports = {
 
   data: "data", // data directory name
 
-  db: "mongodb://localhost:27017/travlr" // db connection url
+  db: "mongodb://127.0.0.1:27017/travlr" // db connection url
 };
